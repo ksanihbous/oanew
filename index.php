301dbb8b5ce8c9a0559d0c9e1b3212d82f675096
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2019-03-18 06:11:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -95,12 +95,12 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-#$key = '30c8be07-9682-42c5-aea1-1f978433fb05'; //API SimSimi
-$url = 'http://ryns-api.herokuapp.com/pandorachat?text='.$pesan;
+$key = '30c8be07-9682-42c5-aea1-1f978433fb05'; //API SimSimi
+$url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
-$diterima = $url['text'];
-if($message['answer']=='text')
+$diterima = $url['response'];
+if($message['type']=='text')
 {
 if($url['result'] == 200)
 	{
